0362c4cde73f98f33ddf23161f5065305f0a3ff8
wjgu604 <arkroyal604@gmail.com>
2024-11-13 21:25:22 +0000

    Update emd_period_energy.py

--- confEMD/emd_period_energy.py
+++ confEMD/emd_period_energy.py
@@ -159,7 +159,6 @@ def emd_period_energy(s, t, plot_spectrum=False):
         global_ws: list
         best_fit: list
         
-    tmp = 3
         
     emd_period_energy = emd_period_energy(energy, dominant_period, dominant_period_err, period, global_ws, best_fit)
     
