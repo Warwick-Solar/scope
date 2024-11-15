39b2298773d3bf6535522a475d7b361d59314942
BelovSA <Sergey.Belov@warwick.ac.uk>
2024-11-15 16:05:30 +0000

    Update plot.py
    - Rename plot_spectrum to plot_fft_spectrum and update it
    - Create plote_modes function to plot emd modes
    - Create plot_emd_spectrum to plot EMD spectrum and confidence limits together

--- confEMD/plot.py
+++ confEMD/plot.py
@@ -6,9 +6,12 @@ Created on Thu Nov 14 13:43:19 2024
 @author: u2273880
 """
 
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 
-def plot_spectrum(fit_fft):
+
+def plot_fft_spectrum(fit_fft):
     """
     Plots the FFT spectrum from fitting results, displaying the power, the model fit,
     and confidence limits.
@@ -56,6 +59,9 @@ def plot_spectrum(fit_fft):
 
     #confidence probability
     prob = fit_fft['conf_prob']
+    
+    #energy relation
+    e_rel = fit_fft['white_energy'] / fit_fft['color_energy']
 
     plt.loglog(period, fit_fft['power'], linewidth=2)
     plt.loglog(period, fit_fft['expectation_continuous'],
@@ -63,13 +69,13 @@ def plot_spectrum(fit_fft):
                color='blue', linewidth=3)
     plt.loglog(period, fit_fft['confidence_limit'], label=f'{prob*100:.0f}%',
                color='red', linewidth=3)
-    plt.title('FFT Spectrum')
+    plt.title(fr'FFT Spectrum ($E_w / E_c =${e_rel:.2f})')
     plt.xlabel('Period [a.u.]')
     plt.ylabel('Fourier Magnitude [a.u.]')
     plt.legend()
     plt.show()
     
-def plot_signal(t, x):
+def plot_signal(t, x, title='Input signal'):
     """
     Plots the input signal over time.
 
@@ -95,5 +101,96 @@ def plot_signal(t, x):
     plt.plot(t, x)
     plt.xlabel('Time')
     plt.ylabel('Signal')
-    plt.title('Input Signal')
+    plt.title(title)
+    plt.show()
+    
+
+def plot_modes(t, modes):
+    """
+    Plots the modes obtained using Empirical Mode Decomposition (EMD) as a series
+    of subplots, each representing one mode over time.
+
+    Parameters
+    ----------
+    t : anumpy array
+        Time values for each mode, used as the x-axis in each subplot.
+    modes : array-like, shape (n_samples, n_modes)
+        2D array where each column represents a mode obtained from EMD, with
+        `n_samples` time points and `n_modes` modes.
+
+    Returns
+    -------
+    None
+    This function does not return any values. It displays a series of subplots,
+    with each subplot representing one mode as a function of time.
+
+    Notes
+    -----
+    Each mode is plotted in its own subplot, aligned vertically in a single column.
+    The x-axis represents time, while the y-axis shows the amplitude of each mode.
+    The y-limits of each subplot are set based on the global minimum and maximum
+    across all modes, ensuring consistent scaling.
+    """
+    
+    num_modes = modes.shape[1]
+    
+    global_min, global_max = np.min(modes), np.max(modes)
+    
+    # Set the figure size to increase the width of the plot
+    plt.figure(figsize=(10, 2 * num_modes)) 
+
+
+    for i in range(num_modes):
+        plt.subplot(num_modes, 1, i + 1)
+        plt.plot(t, modes[:, i])
+        plt.ylabel(f'Mode {i + 1}')
+        plt.ylim(global_min, global_max)
+
+    plt.xlabel('Time')
+    plt.tight_layout()
+    plt.show()
+    
+def plot_emd_spectrum(emd_sp, cutoff_period, conf_period=None, conf_up=None,
+                      conf_down=None, conf_mean=None, fap=None):
+
+    plt.errorbar(emd_sp['period'], emd_sp['energy'], xerr=emd_sp['period_err'],
+                 label='EMD Spectrum', fmt='.', color='orange',
+                 ms=15, capsize=5, mew=2)
+    plt.axvline(x = cutoff_period, color = 'black', linestyle='dashed')
+    
+    if fap is not None:
+        prob = 1 - fap
+        label = f'{prob*100:.0f}%'
+    else:
+        label = ''
+    
+    if conf_period is not None:
+        if conf_up is not None:
+            plt.plot(conf_period, conf_up, color='red', label=label)
+            
+            # get upper boundaries for the periods of the EMD modes found
+            f = interp1d(conf_period, conf_up)
+            upper_bounds = f(emd_sp['period'])
+            mask = emd_sp['energy'] > upper_bounds
+            
+            # mark signigicant EMD modes
+            plt.errorbar(emd_sp['period'][mask], emd_sp['energy'][mask],
+                         xerr=emd_sp['period_err'][mask],
+                         label='Signigicant modes', fmt='.', color='green',
+                         ms=15, capsize=5, mew=2)
+            
+        if conf_down is not None:
+            plt.plot(conf_period, conf_down, color='red')
+        if conf_mean is not None:
+            plt.plot(conf_period, conf_mean, color='blue')
+
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.ylim(1e-3, 1e1)
+    plt.xlim(0.1, 60)
+    plt.title('EMD Spectrum')
+    plt.xlabel('Period [a.u.]')
+    plt.ylabel('EMD Modal Energy [a.u.]')
+    plt.legend()
+    plt.grid()
     plt.show()
