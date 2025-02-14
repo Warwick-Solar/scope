6840b3392987413b821715abb489d4bab7fbbba4
BelovSA <Sergey.Belov@warwick.ac.uk>
2025-02-14 16:29:36 +0000

    Small changes in the example

--- examples/emd_example.py
+++ examples/emd_example.py
@@ -6,36 +6,25 @@ from scope.fourier import fit_fourier
 from scope.emd import emd_modes, emd_trend, emd_energy_spectrum, emd_noise_conf
 from scope.utils import plot_modes, plot_signal, plot_fft_spectrum, plot_emd_spectrum
 
+use_example_data = True # set to False for a new generated time series
 
-# #Generate time series
-# L = 30 #length of time series
-# N = 300 #number of data points 
-# dt = L / N 
+if use_example_data:
+    #Load example data file
+    t, trend, signal, x = np.loadtxt('example_data.txt')
+else:
+    #Generate time series
+    L = 30 #length of time series
+    N = 300 #number of data points 
+    dt = L / N 
 
-# t = dt * np.arange(N)
-# # start_time = np.max(t) * 0.05 
-# decay_time = np.max(t) * 0.3
+    t = dt * np.arange(N)
+    decay_time = np.max(t) * 0.3
 
-# trend = np.exp(-t/decay_time)
-# signal = 0.1 * np.sin(2*np.pi*(1/6)*t) #freq=5/30
-# noise = 0.06 * cn.powerlaw_psd_gaussian(0, t.size) + 0.1 * cn.powerlaw_psd_gaussian(2, t.size)
+    trend = np.exp(-t/decay_time)
+    signal = 0.1 * np.sin(2*np.pi*(1/3)*t) #freq=5/30
+    noise = 0.06 * cn.powerlaw_psd_gaussian(0, t.size) + 0.1 * cn.powerlaw_psd_gaussian(2, t.size)
 
-# x = trend + signal + noise
-
-# plt.plot(t, x)
-# plt.plot(t, trend)
-# plt.plot(t, trend+signal)
-# plt.xlabel('Time')
-# plt.ylabel('Amplitude')
-# plt.title('Input Signal')
-# plt.show()
-
-# np.savetxt('examples/example data', (t, trend, signal, x))
-
-
-#%%############################################################################
-#Load example data file
-t, trend, signal, x = np.loadtxt('examples/example data')
+    x = trend + signal + noise
 
 plt.plot(t, x)
 plt.plot(t, trend)
@@ -46,13 +35,12 @@ plt.title('Input Signal')
 plt.show()
 
 #%%############################################################################
-
 dt = t[1]-t[0]
 N = len(x) #number of data points 
 x -= np.mean(x) #set mean to zero
 
 #Calculate EMD modes and trend
-modes = emd_modes(x, sd_thresh=1e-4)
+modes = emd_modes(x, sd_thresh=0.0001)
 
 #plot modes
 plot_modes(t, modes)
@@ -60,17 +48,16 @@ plot_modes(t, modes)
 #Calculate trend (rough)
 modes = emd_trend(modes, t)
 trend_emd = modes[:, -1]
-plot_signal(t, trend_emd, 'Trend of the signal')
+
+fig, ax = plt.subplots(1)
+ax.plot(t, x, alpha=0.8)
+plot_signal(t, trend_emd, 'Trend of the signal', ax=ax)
 
 #subtract this trend from the signal 
 x = x - trend_emd
 
 #plot detrended signal
 plot_signal(t, x, 'Detrended signal')
-
-#plot updated modes
-plot_modes(t, modes)
-
 #%%############################################################################
 
 #Estimate noise parameters from FFT of the detrended signal
@@ -87,7 +74,7 @@ plot_fft_spectrum(fit_fft)
 #%%############################################################################
 
 #Calculate EMD power spectrum
-emd_sp = emd_energy_spectrum(modes, t, plot_fitting=True)
+emd_sp = emd_energy_spectrum(modes, t, plot_fitting=False)
 
 cutoff_period = 0.4 * len(x) * dt #show cutoff period
 
@@ -101,13 +88,19 @@ fap = 0.05
 
 #Confidence limits for coloured noise
 conf_c = emd_noise_conf(t, alpha=alpha, period_min=2*dt, 
-                        period_max=N*dt, num_samples=100, 
+                        period_max=N*dt, num_samples=300, 
                         signal_energy=fit_fft['color_energy'], fap=fap)
 #Confidence limits for white noise
 if fit_fft['white_energy'] > 0: # check if there is only colored noise model
     conf_w = emd_noise_conf(t, alpha=0, period_min=2*dt,
-                            period_max=N*dt, num_samples = 100, 
+                            period_max=N*dt, num_samples = 300, 
                             signal_energy=fit_fft['white_energy'], fap=fap)
+else:
+    size = len(conf_c['up'])
+    conf_w = {}
+    conf_w['up'] = np.zeros(size)
+    conf_w['down'] = np.zeros(size)
+    conf_w['mean_energy'] = np.zeros(size)
 
 #Upper confidence limit for the combined noises
 conf_up = conf_c['up'] + conf_w['up']
