9fdd78b07a1fa962426d3267347ed34ca4385952
BelovSA <Sergey.Belov@warwick.ac.uk>
2025-08-01 15:21:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
        'colorednoise>=2.2.0',
        'emd>=0.7.0',
-       'numpy>=1.26.4',
+       'numpy>=1.3.0',
        'lmfit>=1.3.2'
     ],
     extras_require={
